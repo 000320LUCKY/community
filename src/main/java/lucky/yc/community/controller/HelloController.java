c25368b2c0d670f65a3f1dc487aecb0fbefbb5a7
LuckyLi <1062721727@qq.com>
2021-03-29 21:22:02 +0800

    init repo

---
+++ src/main/java/lucky/yc/community/controller/HelloController.java
@@ -0,0 +1,24 @@
+package lucky.yc.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hellos")
+
+    public String hello(@RequestParam(name = "name") String name1,Model model) {
+        model.addAttribute("name", name1);
+        return "hello";
+    }
+
+//    @GetMapping("/greeting")
+//    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+//        model.addAttribute("name", name);
+//        return "greeting";
+//    }
+}
---
+++ src/test/java/lucky/yc/community/CommunityApplicationTests.java
@@ -0,0 +1,14 @@
+package lucky.yc.community;
+
+//import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
