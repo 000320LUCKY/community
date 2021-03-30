aeeffffe8032bf27069200aeb0aa406ab80a8662
LuckyLi <1062721727@qq.com>
2021-03-30 20:06:10 +0800

    add index and boots

--- src/main/java/lucky/yc/community/controller/HelloController.java
+++ src/main/java/lucky/yc/community/controller/HelloController.java
@@ -3,22 +3,14 @@ package lucky.yc.community.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
-    @GetMapping("/hellos")
-
-    public String hello(@RequestParam(name = "name") String name1,Model model) {
-        model.addAttribute("name", name1);
-        return "hello";
+    @GetMapping("/")
+    public String index() {
+        return "index";
     }
 
-//    @GetMapping("/greeting")
-//    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-//        model.addAttribute("name", name);
-//        return "greeting";
-//    }
 }
