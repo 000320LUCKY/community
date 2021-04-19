8a5d4c59576bfa48e852db88a7b75f75c75b0899
LuckyLi <1062721727@qq.com>
2021-04-19 10:10:16 +0800

    拦截器静态资源加载修复

--- src/main/java/lucky/yc/community/interceptor/WebConfig.java
+++ src/main/java/lucky/yc/community/interceptor/WebConfig.java
@@ -1,31 +1,39 @@
 package lucky.yc.community.interceptor;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-
     @Autowired
     private SessionInterceptor sessionInterceptor;
 
+    /**
+     * addPathPatterns 添加拦截规则
+     * excludePathPatterns 排除拦截规则
+     *
+     * @param registry
+     */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         //拦截地址参数
         registry.addInterceptor(sessionInterceptor).addPathPatterns("/**");
     }
 
-//    放行静态资源
+
+    //静态资源加载地址
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**","classpath:/META-INF/resources/")
-                .addResourceLocations("classpath:/public/","classpath:/static/")
-                .setCachePeriod(31556926);
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
     }
+
 }
 
